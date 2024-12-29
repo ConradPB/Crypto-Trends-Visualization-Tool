e65a84d8e92e347a5f7e2b03a83bab052d77dded
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-12-29 12:58:52 +0300

    updated

--- backend/src/routes/cryptoRoutes.ts
+++ backend/src/routes/cryptoRoutes.ts
@@ -4,10 +4,10 @@ import { getCryptoPrices } from '../controllers/cryptoController';
 const router = Router();
 
 
-router.use((req, res, next) => {
-    console.log(`Crypto route handler: ${req.method} ${req.url}`);
-    next();
-});
+// router.use((req, res, next) => {
+  // console.log(`Crypto route handler: ${req.method} ${req.url}`);
+   // next();
+//}); 
 
 // Log when the route is hit
 router.get('/prices', (req, res, next) => {
