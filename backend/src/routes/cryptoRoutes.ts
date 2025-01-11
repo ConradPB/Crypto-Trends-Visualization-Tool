40cf590d3a4004f5dc0c4c0a3a3ba672644ce499
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-01-12 01:48:47 +0300

    updated with getTrendingCoins

--- backend/src/routes/cryptoRoutes.ts
+++ backend/src/routes/cryptoRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getCryptoPrices, getHistoricalData, } from '../controllers/cryptoController';
+import { getCryptoPrices, getHistoricalData, getTrendingCoins, } from '../controllers/cryptoController';
 
 const router: Router = Router();
 
 router.get('/prices', getCryptoPrices);
 router.get('/historical', getHistoricalData);
+router.get('/trending', getTrendingCoins);
+
 
 
 export default router;
