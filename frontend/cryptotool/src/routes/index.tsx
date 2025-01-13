11e8fe4f8377955ca759caa0de7bc9f79e6d463f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-01-14 00:30:04 +0300

    set up index

---
+++ frontend/cryptotool/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import NotFound from '../pages/NotFound';
+
+const AppRoutes = () => (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    </Router>
+);
+
+export default AppRoutes;
