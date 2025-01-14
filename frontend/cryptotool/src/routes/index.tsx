92a5cafd8de889f410c39929e4bd417cb62b2de2
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-01-15 01:54:48 +0300

    updated with historical data

--- frontend/cryptotool/src/routes/index.tsx
+++ frontend/cryptotool/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import NotFound from '../pages/NotFound';
 import CryptoPrices from '../components/CryptoPrices';
+import HistoricalData from '../components/HistoricalData';
 
 const AppRoutes = () => (
     <Router>
@@ -9,6 +10,8 @@ const AppRoutes = () => (
             <Route path="/" element={<Dashboard />} />
             <Route path="*" element={<NotFound />} />
             <Route path="/crypto" element={<CryptoPrices />} />
+            <Route path="/historical" element={<HistoricalData />} />
+
         </Routes>
     </Router>
 );
