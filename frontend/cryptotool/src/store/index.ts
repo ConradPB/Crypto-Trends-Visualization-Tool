f9dd5bcc30bc2b6881e74284fe72b2edc78e6eb7
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-01-14 14:45:34 +0300

    updated

--- frontend/cryptotool/src/store/index.ts
+++ frontend/cryptotool/src/store/index.ts
@@ -1,15 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import cryptoReducer from '../features/cryptoSlice';
 
-export const store = configureStore({
-    reducer: {
-        crypto: cryptoReducer,
-    },
+const store = configureStore({
+  reducer: {
+    crypto: cryptoReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export default store;
