e4450131a725070c722aa561f89d9c5de131c1db
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-01-14 18:10:29 +0300

    updated

--- frontend/cryptotool/src/components/HistoricalData.tsx
+++ frontend/cryptotool/src/components/HistoricalData.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchHistoricalData } from "../features/cryptoSlice";
 import { RootState } from "../store";
+import { Box, FormControl, InputLabel, MenuItem, Select, Typography } from "@mui/material";
 
 
 const HistoricalData: React.FC = () => {
@@ -19,3 +20,37 @@ const HistoricalData: React.FC = () => {
     const formattedData = historicalData[selectedCoin]?.prices?.map(([timestamp, price]: [number, number]) => ({
         date: new Date(timestamp).toLocaleDateString(), price,
     })) || [];
+
+    return (
+        <Box padding={4}>
+          <Typography variant="h4" gutterBottom>
+            Historical Data
+          </Typography>
+    
+          <FormControl sx={{ minWidth: 200, marginRight: 2 }}>
+            <InputLabel id="coin-select-label">Cryptocurrency</InputLabel>
+            <Select
+              labelId="coin-select-label"
+              value={selectedCoin}
+              onChange={(e) => setSelectedCoin(e.target.value)}
+            >
+              <MenuItem value="bitcoin">Bitcoin</MenuItem>
+              <MenuItem value="ethereum">Ethereum</MenuItem>
+            </Select>
+          </FormControl>
+    
+          <FormControl sx={{ minWidth: 150 }}>
+            <InputLabel id="time-range-select-label">Time Range</InputLabel>
+            <Select
+              labelId="time-range-select-label"
+              value={timeRange}
+              onChange={(e) => setTimeRange(e.target.value)}
+            >
+              <MenuItem value="7">7 Days</MenuItem>
+              <MenuItem value="30">30 Days</MenuItem>
+            </Select>
+          </FormControl>
+    
+        
+    
+    export default HistoricalData;
