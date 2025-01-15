2e1923bbe2c08176920f8e725858f1b317e1b6bc
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-01-15 14:49:00 +0300

    updated

--- frontend/cryptotool/src/routes/index.tsx
+++ frontend/cryptotool/src/routes/index.tsx
@@ -3,6 +3,7 @@ import Dashboard from '../pages/Dashboard';
 import NotFound from '../pages/NotFound';
 import CryptoPrices from '../components/CryptoPrices';
 import HistoricalData from '../components/HistoricalData';
+import TrendingCoins from '../pages/TrendingCoins';
 
 const AppRoutes = () => (
     <Router>
@@ -10,6 +11,7 @@ const AppRoutes = () => (
             <Route path="/" element={<Dashboard />} />
             <Route path="*" element={<NotFound />} />
             <Route path="/crypto" element={<CryptoPrices />} />
+            <Route path="/trending" element={<TrendingCoins />} />
             <Route path="/historical" element={<HistoricalData />} />
 
         </Routes>
