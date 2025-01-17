4797190425ef31a3366943b95cf274fae6eb5fc8
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-01-17 21:47:19 +0300

    updated prices

--- frontend/cryptotool/src/components/CryptoPrices.tsx
+++ frontend/cryptotool/src/components/CryptoPrices.tsx
@@ -1,9 +1,8 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch } from "../store";
 import { fetchCryptoPrices } from "../features/cryptoSlice";
-import { RootState } from "../store";
-import { Box, Typography } from "@mui/material";
+import { RootState, AppDispatch } from "../store";
+import { Box, Typography, CircularProgress } from "@mui/material";
 
 const CryptoPrices = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -12,33 +11,48 @@ const CryptoPrices = () => {
   );
 
   useEffect(() => {
-    dispatch(fetchCryptoPrices("bitcoin,ethereum")); // Include the required coins
+    dispatch(fetchCryptoPrices("bitcoin,ethereum"));
   }, [dispatch]);
 
   if (loading) {
-    return <Typography>Loading...</Typography>;
+    return (
+      <Box display="flex" justifyContent="center" mt={4}>
+        <CircularProgress />
+      </Box>
+    );
   }
 
   if (error) {
-    return <Typography color="error">{error}</Typography>;
+    return (
+      <Typography color="error" mt={4} textAlign="center">
+        {error}
+      </Typography>
+    );
+  }
+
+  if (Object.keys(prices).length === 0) {
+    return (
+      <Typography mt={4} textAlign="center">
+        No prices available
+      </Typography>
+    );
   }
 
   return (
-    <Box>
-      <Typography variant="h5">Cryptocurrency Prices</Typography>
-      {Object.entries(prices).length === 0 ? (
-        <Typography>No data available</Typography>
-      ) : (
-        <Box>
-          {Object.entries(prices).map(([coin, priceData]) => (
-            <Typography key={coin}>
-              {coin}: ${priceData}
-            </Typography>
-          ))}
-        </Box>
-      )}
+    <Box mt={4}>
+      <Typography variant="h5" textAlign="center" mb={2}>
+        Cryptocurrency Prices
+      </Typography>
+      <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
+        {Object.entries(prices).map(([coin, price]) => (
+          <Typography key={coin}>
+            {coin.toUpperCase()}: ${price.toFixed(2)}
+          </Typography>
+        ))}
+      </Box>
     </Box>
   );
+  
 };
 
 export default CryptoPrices;
