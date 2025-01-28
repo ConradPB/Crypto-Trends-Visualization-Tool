fdd5a7822768df7f676fb7bb474aab8c2727fccd
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-01-28 23:53:25 +0300

    updated

--- frontend/cryptotool/src/pages/TrendingCoins.tsx
+++ frontend/cryptotool/src/pages/TrendingCoins.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Box, Card, Typography, CircularProgress } from "@mui/material";
 import { fetchTrendingCoins } from "../features/cryptoSlice";
