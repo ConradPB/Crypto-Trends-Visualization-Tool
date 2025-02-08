5c1488cad181dbbad9d3e8551c16a935e44d1a9f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-08 23:54:13 +0300

    update/creating connection to axios

--- frontend/cryptotool/src/components/CryptoPrices.tsx
+++ frontend/cryptotool/src/components/CryptoPrices.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCryptoPrices } from "../features/cryptoSlice";
 import { RootState, AppDispatch } from "../store";
+import axiosInstance from "../api/axiosInstance";
 import {
   Box,
   Typography,
