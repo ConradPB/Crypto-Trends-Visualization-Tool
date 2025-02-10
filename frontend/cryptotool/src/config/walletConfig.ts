e527f629784ea5b49283faa0d39b518d427c246f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-10 23:51:07 +0300

    updated addresses

--- frontend/cryptotool/src/config/walletConfig.ts
+++ frontend/cryptotool/src/config/walletConfig.ts
@@ -1,5 +1,6 @@
 export const WALLET_ADDRESSES = {
-    bitcoin: "",
-    ethereum: "",
-    usdt: "",
+    bitcoin: "bc1qcajrajvlnklqnkrpmem7sq5y2nj9he9ru3ajqh",
+    ethereum: "0x1b6D6A00F754a0d5068449Fe55f1A7839733BB42",
+    usdt: "0x1b6D6A00F754a0d5068449Fe55f1A7839733BB42",
+    xrp: "rMFtpUiAr1rd6EyjS9Xswb17Ajs8KKycsS",
   };
