d3dc5f41f3fc33a58739de35407afd1a5d63ce44
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-11 13:16:04 +0300

    updated

--- frontend/cryptotool/src/routes/index.tsx
+++ frontend/cryptotool/src/routes/index.tsx
@@ -1,24 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from '../pages/Dashboard';
-import NotFound from '../pages/NotFound';
-import CryptoPrices from '../components/CryptoPrices';
-import HistoricalData from '../components/HistoricalData';
-import TrendingCoins from '../pages/TrendingCoins';
-import PriceAlerts from '../components/PriceAlerts';
-import AlertHistory from '../components/AlertHistory';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import NotFound from "../pages/NotFound";
+import CryptoPrices from "../components/CryptoPrices";
+import HistoricalData from "../components/HistoricalData";
+import TrendingCoins from "../pages/TrendingCoins";
+import PriceAlerts from "../components/PriceAlerts";
+import AlertHistory from "../components/AlertHistory";
+import SupportMe from "../pages/SupportMe";
 
 const AppRoutes = () => (
-    <Router>
-        <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/alerts" element={<PriceAlerts />} />
-            <Route path="/crypto" element={<CryptoPrices />} />
-            <Route path="/trending" element={<TrendingCoins />} />
-            <Route path="/historical" element={<HistoricalData />} />
-            <Route path="/alerts-history" element={<AlertHistory />} />
-            <Route path="*" element={<NotFound />} /> {/* Move this to the end */}
-        </Routes>
-    </Router>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/alerts" element={<PriceAlerts />} />
+      <Route path="/crypto" element={<CryptoPrices />} />
+      <Route path="/support-me" element={<SupportMe />} />
+      <Route path="/trending" element={<TrendingCoins />} />
+      <Route path="/historical" element={<HistoricalData />} />
+      <Route path="/alerts-history" element={<AlertHistory />} />
+      <Route path="*" element={<NotFound />} /> {/* Move this to the end */}
+    </Routes>
+  </Router>
 );
 
 export default AppRoutes;
