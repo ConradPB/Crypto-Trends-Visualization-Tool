0ec15208eca255858dd785bdc8f51e005e1bedcb
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-11 16:12:01 +0300

    updated

---
+++ frontend/cryptotool/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+// src/components/Navbar.tsx
+import React from "react";
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+
+const Navbar = () => {
+  return (
+    
+  );
+};
+
+export default Navbar;
