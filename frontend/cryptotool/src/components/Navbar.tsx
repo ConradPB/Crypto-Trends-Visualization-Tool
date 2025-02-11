016a6376680cd3d15db5ccac7eed06ae22beb8c5
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-11 16:24:46 +0300

    updated

--- frontend/cryptotool/src/components/Navbar.tsx
+++ frontend/cryptotool/src/components/Navbar.tsx
@@ -5,7 +5,34 @@ import HomeIcon from "@mui/icons-material/Home";
 
 const Navbar = () => {
   return (
-    
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        {/* Home Button */}
+        <IconButton
+          component={Link}
+          to="/"
+          color="inherit"
+          aria-label="Home"
+          sx={{ mr: 2 }}
+        >
+          <HomeIcon />
+        </IconButton>
+
+        {/* App Title */}
+        <Typography
+          variant="h6"
+          component={Link}
+          to="/"
+          sx={{
+            flexGrow: 1,
+            textDecoration: "none",
+            color: "inherit",
+          }}
+        >
+          Crypto Trends Tool
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
 
