108ddb8891d969c909dc0be1fb5c3c2eac5154d5
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-14 01:19:18 +0300

    updated

---
+++ frontend/cryptotool/src/__tests__/PriceAlerts.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import { screen } from "@testing-library/react";
+import PriceAlerts from "../components/PriceAlerts";
+import "@testing-library/jest-dom";
+
+test("renders Price Alerts heading", () => {
+  render(<PriceAlerts />);
+  const heading = screen.getByText(/Price Alerts/i);
+  expect(heading).toBeInTheDocument();
+});
