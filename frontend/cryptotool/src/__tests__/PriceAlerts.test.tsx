42fe0cf03a8d5c11720046b3890e7f16c3263ef7
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-14 18:51:07 +0300

    updated

--- frontend/cryptotool/src/__tests__/PriceAlerts.test.tsx
+++ frontend/cryptotool/src/__tests__/PriceAlerts.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import { Provider } from "react-redux";
 import PriceAlerts from "../components/PriceAlerts";
 import { setupStore } from "../store";
