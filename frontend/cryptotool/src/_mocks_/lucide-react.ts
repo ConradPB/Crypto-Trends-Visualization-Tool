9356c9bd128c0110d94b297105c043676a49ec39
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-15 22:30:18 +0300

    updated

--- frontend/cryptotool/src/_mocks_/lucide-react.ts
+++ frontend/cryptotool/src/_mocks_/lucide-react.ts
@@ -1,3 +1,5 @@
-export const Bell = 'Bell';
-export const BellOff = 'BellOff';
-export const Trash2 = 'Trash2';
+import React from 'react';
+
+export const Bell = () => React.createElement('div', { 'data-testid': 'bell-icon' });
+export const BellOff = () => React.createElement('div', { 'data-testid': 'bell-off-icon' });
+export const Trash2 = () => React.createElement('div', { 'data-testid': 'trash-icon' });
