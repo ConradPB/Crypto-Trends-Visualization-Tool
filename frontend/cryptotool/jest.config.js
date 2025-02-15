b2bb95bfd8eab0fb36c2cdaf2e846352801863c1
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-15 21:59:38 +0300

    updated

--- frontend/cryptotool/jest.config.js
+++ frontend/cryptotool/jest.config.js
@@ -2,16 +2,16 @@ export default {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-      'lucide-react': '<rootDir>/src/__mocks__/lucide-react.ts'  // Changed this line
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^lucide-react$': '<rootDir>/src/__mocks__/lucide-react.ts' 
     },
     transform: {
-      '^.+\\.(ts|tsx)$': ['ts-jest', {
-        tsconfig: 'tsconfig.test.json',
-        useESM: true
-      }]
+        '^.+\\.(ts|tsx)$': ['ts-jest', {
+            tsconfig: 'tsconfig.test.json',
+            useESM: true
+        }]
     },
     extensionsToTreatAsEsm: ['.ts', '.tsx'],
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-  }
+}
