f1253597c31735c31ebe3f018e9e042221ad081c
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-15 22:54:31 +0300

    updated

--- frontend/cryptotool/src/_tests_/PriceAlerts.test.tsx
+++ frontend/cryptotool/src/_tests_/PriceAlerts.test.tsx
@@ -4,21 +4,25 @@ import { Provider } from "react-redux";
 import PriceAlerts from "../components/PriceAlerts";
 import { setupStore } from "../store";
 
+const mockInitialState = {
+  alerts: {
+    alerts: [],
+    history: [],
+  },
+  crypto: {
+    prices: {
+      bitcoin: { usd: 50000 },
+    },
+    historicalData: {},
+    trendingCoins: [],
+    loading: false,
+    error: null,
+  },
+};
+
 describe("PriceAlerts Component", () => {
   it("renders without crashing", () => {
-    const store = setupStore({
-      alerts: {
-        alerts: [],
-        history: [],
-      },
-      crypto: {
-        prices: {},
-        historicalData: {},
-        trendingCoins: [],
-        loading: false,
-        error: null,
-      },
-    });
+    const store = setupStore(mockInitialState);
 
     render(
       <Provider store={store}>
