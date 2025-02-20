c88f6a3d0435acfa6392fa56cf96e839c9782947
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-20 22:17:48 +0300

    updated/optimized for vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -42,12 +42,4 @@ if (process.env.NODE_ENV !== "production") {
   );
 }
 
-// Don't start server in production (Vercel)
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
-
 export default app;
