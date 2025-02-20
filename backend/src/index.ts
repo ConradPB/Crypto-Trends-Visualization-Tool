71ea12c6b4097bf96d1865121a7c3547999b0513
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-20 23:17:59 +0300

    updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import { default as express } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cryptoRoutes from "./routes/cryptoRoutes";
