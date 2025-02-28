973681149bbbe65dbc3fc4f331b38a9288b8a54f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-28 14:11:25 +0300

    updated

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,10 +1,11 @@
 import { createLogger, format, transports } from "winston";
+import { TransformableInfo } from "logform";
 
 const logger = createLogger({
   level: "info",
   format: format.combine(
-    format.timestamp(),
-    format.printf((info) => {
+    format.timestamp(), // Adds timestamp
+    format.printf((info: TransformableInfo & { timestamp: string }) => {
       return `[${info.timestamp}] ${info.level.toUpperCase()}: ${info.message}`;
     })
   ),
