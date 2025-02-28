447d645dad0fa5487b84aef99c08b6d744786983
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-28 18:24:59 +0300

    updated url

--- frontend/cryptotool/src/api/axiosInstance.ts
+++ frontend/cryptotool/src/api/axiosInstance.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'crypto-trends-visualization-tool-9af51bhkc-conrad-p-bs-projects.vercel.app', 
-    timeout: 10000,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: "https://crypto-tool-weld.vercel.app",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosInstance;
