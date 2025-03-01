401830d49b366e84638f1acbb75375a7419b07a3
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-03-01 13:41:07 +0300

    updated url for vercel

--- frontend/cryptotool/src/api/axiosInstance.ts
+++ frontend/cryptotool/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://crypto-tool-weld.vercel.app",
+  baseURL: "https://crypto-tool-weld.vercel.app/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
