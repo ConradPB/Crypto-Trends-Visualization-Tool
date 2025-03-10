d129a74b4f2269502ee9d3649a6f6cacc0750081
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-03-11 02:19:13 +0300

    Point FE to new live BE: crypto-trends-visualization-tool.vercel.app

--- frontend/cryptotool/src/api/axiosInstance.ts
+++ frontend/cryptotool/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "",
+  baseURL: "https://crypto-trends-visualization-tool.vercel.app/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
