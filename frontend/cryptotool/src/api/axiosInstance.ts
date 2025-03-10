4335d4f93fcc0836b5882b7cd5e8d0b1beea5df9
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-03-10 20:45:44 +0300

    updated

--- frontend/cryptotool/src/api/axiosInstance.ts
+++ frontend/cryptotool/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "crypto-tool-aaszkvsck-conrad-p-bs-projects.vercel.app/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
